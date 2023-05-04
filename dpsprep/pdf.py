841d7203835e2849e7303145191e2135538df7b8
Ianis Vasilev <ianis@ivasilev.net>
2023-05-04 05:04:39 +0300

    Fix added outline getting ignored

--- dpsprep/pdf.py
+++ dpsprep/pdf.py
@@ -5,12 +5,13 @@ from .workdir import WorkingDirectory
 
 def combine_pdfs_on_fs(workdir: WorkingDirectory, outline: pdfrw.IndirectPdfDict):
     text_pdf = pdfrw.PdfReader(workdir.text_pdf_path)
+    writer = pdfrw.PdfWriter()
 
     for i, page in enumerate(text_pdf.pages):
         merger = pdfrw.PageMerge(page)
         image_pdf = pdfrw.PdfReader(workdir.get_page_pdf_path(i))
         merger.add(image_pdf.pages[0]).render()
+        writer.addpage(page)
 
-    writer = pdfrw.PdfWriter()
     writer.trailer.Root.Outlines = outline
-    writer.write(workdir.dest, text_pdf)
+    writer.write(workdir.dest)

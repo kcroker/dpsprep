6b92bdac2f99fea8beafa2dca5d53d88f8c395f0
Ianis Vasilev <ianis@ivasilev.net>
2023-10-28 14:24:06 +0300

    Add options for disabling the text layer and for directly running OCR

--- dpsprep/pdf.py
+++ dpsprep/pdf.py
@@ -3,8 +3,8 @@ import pdfrw
 from .workdir import WorkingDirectory
 
 
-def combine_pdfs_on_fs(workdir: WorkingDirectory, outline: pdfrw.IndirectPdfDict):
-    text_pdf = pdfrw.PdfReader(workdir.text_pdf_path)
+def combine_pdfs_on_fs_with_text(workdir: WorkingDirectory, outline: pdfrw.IndirectPdfDict):
+    text_pdf = pdfrw.PdfReader(workdir.text_layer_pdf_path)
     writer = pdfrw.PdfWriter()
 
     for i, page in enumerate(text_pdf.pages):
@@ -15,3 +15,14 @@ def combine_pdfs_on_fs(workdir: WorkingDirectory, outline: pdfrw.IndirectPdfDict
 
     writer.trailer.Root.Outlines = outline
     writer.write(workdir.combined_pdf_path)
+
+
+def combine_pdfs_on_fs_without_text(workdir: WorkingDirectory, outline: pdfrw.IndirectPdfDict, max_page: int):
+    writer = pdfrw.PdfWriter()
+
+    for i in range(max_page):
+        image_pdf = pdfrw.PdfReader(workdir.get_page_pdf_path(i))
+        writer.addpage(image_pdf.pages[0])
+
+    writer.trailer.Root.Outlines = outline
+    writer.write(workdir.combined_pdf_without_text_path)
